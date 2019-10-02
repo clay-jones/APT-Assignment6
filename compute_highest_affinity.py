65e4b77deb2b227987e5bcafadadf385e014b569
[clay-jones] <[clay.jones@utexas.edu]>
2019-10-02 07:42:18 -0500

    step3

---
+++ compute_highest_affinity.py
@@ -0,0 +1,73 @@
+# No need to process files and manipulate strings - we will
+# pass in lists (of equal length) that correspond to
+# sites views. The first list is the site visited, the second is
+# the user who visited the site.
+
+# See the test cases for more details.
+
+def highest_affinity(site_list, user_list, time_list):
+    # Returned string pair should be ordered by dictionary order
+    # I.e., if the highest affinity pair is "foo" and "bar"
+    # return ("bar", "foo").
+
+    user_history = {}
+
+    for site, user in zip(site_list, user_list):
+        if user not in user_history:
+            user_history[user] = set()
+
+        user_history[user].add(site)
+
+    affinities = {}
+    for user, history in user_history.items():
+        history = list(history)
+        history.sort()
+        for i, site1 in enumerate(history):
+            for site2 in history[i+1:]:
+                pair = (site1, site2)
+                if pair not in affinities:
+                    affinities[pair] = 0
+                affinities[pair] += 1
+
+    return max(affinities, key=affinities.get)
+
+def sort_list(input_list):
+    return input_list.sort()
+
+def sort_list_reverse(input_list):
+    return input_list.sort(reverse=True)
+
+def zip_lists(in_list1, in_list2):
+    return zip(in_list1, in_list2)
+
+def is_number_like(x):
+    try:
+        int(x) + 1
+    except:
+        return False
+    return True
+
+def lowest_affinity(site_list, user_list, time_list):
+    # Cheap knock-off of highest_affinitydone to get
+    # testing coverage down
+
+    user_history = {}
+
+    for site, user in zip(site_list, user_list):
+        if user not in user_history:
+            user_history[user] = set()
+
+        user_history[user].add(site)
+
+    affinities = {}
+    for user, history in user_history.items():
+        history = list(history)
+        history.sort()
+        for i, site1 in enumerate(history):
+            for site2 in history[i+1:]:
+                pair = (site1, site2)
+                if pair not in affinities:
+                    affinities[pair] = 0
+                affinities[pair] += 1
+
+    return min(affinities, key=affinities.get)
